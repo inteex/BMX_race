666a3cee3ca6fadedfdab27700d9ad5bf5b52c48
inteex13 <inteex13@gmail.com>
2019-07-11 08:56:29 +0200

    regression on frames indir added!

--- analyse_frame.py
+++ analyse_frame.py
@@ -10,8 +10,9 @@ from sklearn.tree import DecisionTreeRegressor
 import math
 import pickle
 from sklearn.model_selection import train_test_split
-import statsmodels.api as sm
-import matplotlib.pyplot as plt
+
+# import statsmodels.api as sm
+# import matplotlib.pyplot as plt
 
 pd.set_option('max_rows', 50)
 pd.set_option('max_columns', 10)
@@ -19,29 +20,26 @@ pd.set_option('max_columns', 10)
 
 class AnalyseFrame:
 
-    def regression_on_frames(self):
-        frames = pd.read_csv("concatenat/AllframesConcatenated.csv")
+    def regression_on_frames(self, all_frames_file):
+        frames = pd.read_csv(all_frames_file)
         frames = frames.reset_index(drop=True)
         frames = frames.fillna(frames.mean())
-        X = frames.drop(['TimeEnd', 'AlphaGaitDmin', 'DAlignementMin', 'DEpauleMin',
-                         'DistanceRecul', 'Dmin',
-                         'ThetaManivelleDepart', 'TpsReaction',
-                         'moyennePuissance1', 'moyennePuissance2', 'moyennePuissance3',
-                         'moyennePuissance4'], axis=1)
+        X = frames[['Braquet', 'MasseBike', 'MasseRider', 'TailleRider',
+                    'longueurManivelle']]
         y = frames['TimeEnd']
 
         polynomial_features = PolynomialFeatures(degree=2)
         X = polynomial_features.fit_transform(X)
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=13)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=11)
 
-        lm = Ridge()
+        lm = Ridge(1)
         lm.fit(X_train, y_train)
         predictions = lm.predict(X_test)
 
         # save the model to disk
-        # filename = '_frames.sav'
-        # pickle.dump(lm2, open(filename, 'wb'))
+        filename = 'Ridge_deg2_frames.sav'
+        pickle.dump(lm, open(filename, 'wb'))
         print('R2 Ridge :' + str(r2_score(y_test, predictions)))
         print(lm.coef_)
         print()
@@ -57,13 +55,13 @@ class AnalyseFrame:
 
         # y_pred = lm.predict(X_test)
 
-    def model_selection(self):
-        frames = pd.read_csv("C:\\Users\\mekhezzr\\PycharmProjects\\bmx_race\\concatenat\\AllframesConcatenated.csv",
-                             delimiter=',')
+    def model_selection(self, all_frames_file):
+        frames = pd.read_csv(all_frames_file, delimiter=',')
         frames = frames.reset_index(drop=True)
         frames = frames.fillna(frames.mean())
+        frames = frames[['Braquet', 'MasseBike', 'MasseRider', 'TailleRider',
+                         'longueurManivelle', 'TimeEnd']]
         models = [Ridge(alpha=1), LinearRegression(), Lasso(), ElasticNet()]
-
         for model in models:
             print("-" * 50)
             print(model)
@@ -78,7 +76,6 @@ class AnalyseFrame:
                 """
 
                 X = frames.drop(['TimeEnd'], axis=1)
-
                 y = frames['TimeEnd']
                 y = y.fillna(y.mean())
 
@@ -86,17 +83,15 @@ class AnalyseFrame:
                 X = polynomial_features.fit_transform(X)
 
                 # test set
-                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=13)
-
+                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=11)
                 # cross validation set
-                X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.15, random_state=13)
+                X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.15, random_state=11)
 
                 lm = model
                 lm.fit(X_train, y_train)
                 y_pred = lm.predict(X_test)
 
                 y_pred_val = lm.predict(X_val)
-
                 r2_score_test = r2_score(y_test, y_pred)
                 r2_score_val = r2_score(y_val, y_pred_val)
                 # if r2_score_test > 0.78 and r2_score_val > 0.79:
@@ -107,13 +102,12 @@ class AnalyseFrame:
         #
         # print(result.summary())
 
-    def test_model_frames(self):
-        frames = pd.read_csv("C:\\Users\\mekhezzr\\PycharmProjects\\bmx_race\\concatenat\\AllframesConcatenated.csv")
+    def test_model_frames(self, frames_file):
+        frames = pd.read_csv(frames_file)
         frames = frames.reset_index(drop=True)
 
-        X = frames.drop(['TimeEnd','AlphaGaitDmin', 'DAlignementMin', 'DEpauleMin',
-                'DistanceRecul', 'Dmin',
-                'ThetaManivelleDepart',], axis=1)
+        X = frames[['Braquet', 'MasseBike', 'MasseRider', 'TailleRider',
+                    'longueurManivelle', 'TimeEnd']]
         X = X.fillna(frames.mean())
         y = frames['TimeEnd']
         y = y.fillna(y.mean())
@@ -122,13 +116,13 @@ class AnalyseFrame:
         X = polynomial_features.fit_transform(X)
 
         # test set
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=11)
-        lm = Ridge(alpha=1)
-        # lm = pickle.load(open('Ridge_deg2_frames.sav', 'rb'))
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=102)
+        # lm = Ridge(alpha=1)
+        lm = pickle.load(open('Ridge_deg2_frames.sav', 'rb'))
         lm.fit(X_train, y_train)
         y_pred = lm.predict(X_test)
         r2_score_test = r2_score(y_test, y_pred)
-        print(" R2: {} ".format( r2_score_test))
+        print(" R2: {} ".format(r2_score_test))
         # print('test_R2 {}'.format(str(r2_score_test)))
 
         erreur = []
@@ -144,8 +138,8 @@ class AnalyseFrame:
 
 if __name__ == '__main__':
     frame = AnalyseFrame()
-
+    data = 'C:\\Program Files\\Git\\BMX_race\\concatenat\\AllframesConcatenated.csv'
     warnings.filterwarnings("ignore")  # ignore warnings
-    # frame.model_selection()
-    # frame.regression_on_frames()
-    frame.test_model_frames()
+    # frame.model_selection(data)
+    frame.regression_on_frames(data)
+    # frame.test_model_frames(data)
